8d2f6eab64bdc5fdcb23d4421bd162477943b955
ThomasMCarballo <99844259+ThomasMCarballo@users.noreply.github.com>
2022-02-16 19:10:11 -0300

    Create Questão1.js

---
+++ Questão1.js
@@ -0,0 +1,10 @@
+var n = prompt("Digite a quantidade de Linhas:");
+var escada = '', escada1 = '';
+for (var y =1; y<=n; y++){
+ escada = escada + '* ';
+  escada1 = escada1 + '*';
+  if (y<n)
+  console.log(escada)
+  else
+  console.log(escada1)
+}
